03d5e5c4496909c9b17da609625255e57e3ceabe
shunji.tomokane <shu.nji@ezweb.ne.jp>
2020-03-16 10:36:37 +0900

    Modify Index/NewServlet

--- src/models/Task.java
+++ src/models/Task.java
@@ -45,14 +45,8 @@ public class Task {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
-    }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
+    
     public String getContent() {
         return content;
     }
